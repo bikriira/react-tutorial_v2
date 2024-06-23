edb5f52212a3efe99ea2bc12cd97891969623cba
bikriira <bikriiradufasha@gmail.com>
2024-06-23 20:31:20 +0200

    [Learn] useEffect basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,24 @@
-import { useState } from "react";
-import UserDetails from "./components/UserDetails.jsx";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: "Bikri",
-      email: "bikri@gmail.com",
-    },
-    {
-      id: 2,
-      username: "Anson",
-      email: "anson@gmail.com",
-    },
-  ]);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [counter, setCounter] = useState(3);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = "React tutorial " + counter;
+  });
 
   return (
-    <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const newUser = { id: counter, username, email };
+    <div>
+      <div>Youclicked the button {counter} timed!</div>
+      <button
+        onClick={(e) => {
           setCounter(counter + 1);
-
-          setUsers((currentUsersList) => [...currentUsersList, newUser]);
         }}
       >
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <button>Add user</button>
-      </form>
-      <br />
-
-      {users.map((user) => (
-        <UserDetails key={user.id} user={user} setUsers={setUsers} />
-      ))}
-    </>
+        Click me
+      </button>
+    </div>
   );
 }
+
+// Key point: whenever a useState is called a component is re-rendered, and by default useEffect is called
